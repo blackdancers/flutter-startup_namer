7f0dfb79b0b6eb7016f94ce4345a37834427f30d
毒匠 <blackdancers@163.com>
2019-08-12 17:02:47 +0800

    创建一个无限滚动的 ListView

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,63 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  //更新 MyApp 的 build() 方法，修改 title 的值来改变标题，修改 home 的值为 RandomWords widget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
+        title: 'Startup Name Generator',
+        home: RandomWords(),
     );
 
   }
 }
 
 /**
- * 添加一个 stateful widget（有状态的 widget）—— RandomWords，它会创建自己的状态类 —— RandomWordsState，
- * 然后你需要将 RandomWords 内嵌到已有的无状态的 MyApp widget。
+ * 扩展（继承）RandomWordsState 类，以生成并显示单词对列表
  */
 class RandomWordsState extends State<RandomWords> {
+  //添加一个 _suggestions 列表以保存建议的单词对，同时，添加一个 _biggerFont 变量来增大字体大小
+  //在 Dart 语言中使用下划线前缀标识符，会 强制其变成私有
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+
+  //添加 _buildSuggestions() 方法,此方法构建显示建议单词对的 ListView。
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // 该方法通过将生成单词对的代码从 MyApp 移动到 RandomWordsState 来生成单词对。
-    //删除 MyApp 里生成文字的代码
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    //更新 RandomWordsState 的 build 方法以使用 _buildSuggestions()，而不是直接调用单词生成库
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
+
+
+
 }
 
 /**
