f8578bac4978def4e1e7ad95ea756bd45d8c3fa6
liu_debin <liu_debin@finebei.com>
2019-08-12 17:02:03 +0800

    添加一个 Stateful widget（有状态的），

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -21,10 +20,32 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
 
   }
+}
+
+/**
+ * 添加一个 stateful widget（有状态的 widget）—— RandomWords，它会创建自己的状态类 —— RandomWordsState，
+ * 然后你需要将 RandomWords 内嵌到已有的无状态的 MyApp widget。
+ */
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    // 该方法通过将生成单词对的代码从 MyApp 移动到 RandomWordsState 来生成单词对。
+    //删除 MyApp 里生成文字的代码
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+/**
+ * 添加有状态的 RandomWords widget 到 main.dart，RandomWords widget 除了创建 State 类之外几乎没有其他任何东西
+ */
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
 }
